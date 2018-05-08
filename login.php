7780147af293ca70ee2adff8dbe5572c87a66734
annaS <dupa113@dupa.pl>
2018-05-08 14:25:16 +0200

    second commit

--- login.php
+++ login.php
@@ -3,57 +3,13 @@
 
 	if(isset($_POST['login'])) {
 
-        class User {
-            var $username;
-            var $psw;
-            
-            function User ($aa) {
-                foreach ($aa as $k=>$v)
-                    $this->$k = $aa[$k];
-            }
-        }
-
-        function readDatabase($filename) {
-            // read the XML database of Users
-            $data = implode("", file($filename));
-            $parser = xml_parser_create();
-            xml_parser_set_option($parser, XML_OPTION_CASE_FOLDING, 0);
-            xml_parser_set_option($parser, XML_OPTION_SKIP_WHITE, 1);
-            xml_parse_into_struct($parser, $data, $values, $tags);
-            xml_parser_free($parser);
-
-            // loop through the structures
-            foreach ($tags as $key=>$val) {
-                if ($key == "person") {
-                    $molranges = $val;
-                    // each contiguous pair of array entries are the 
-                    // lower and upper range for each molecule definition
-                    for ($i=0; $i < count($molranges); $i+=2) {
-                        $offset = $molranges[$i] + 1;
-                        $length = $molranges[$i + 1] - $offset;
-                        $tdb[] = parseMol(array_slice($values, $offset, $length));
-                    }
-                } else {
-                    continue;
-                }
-            }
-            return $tdb;
-        }
-
-        function parseMol($mvalues) {
-            for ($i=0; $i < count($mvalues); $i++) {
-                $mol[$mvalues[$i]["tag"]] = $mvalues[$i]["value"];
-            }
-            return new User($mol);
-        }
-
-        $db = readDatabase("database.xml");
-
-
-		$usersArray = [];
-		foreach ($db as $key => $value) {
-		    $usersArray[$value->username] = $value->psw;
-		}
+    $users = simplexml_load_file('database.xml');
+    $usersArray = [];
+    for ($i = 0; $i < count($users); $i++) { 
+        $userNew = trim($users->person[$i]->username);
+        $passwordNew = trim($users->person[$i]->psw);
+        $usersArray[$userNew] = $passwordNew;
+    }
 
 		$username = preg_replace('/[^A-Za-z]/' , '', $_POST['username']);
 		$password = $_POST['password'];
