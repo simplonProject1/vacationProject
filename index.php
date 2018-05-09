b7449cc347e391643e69e06ea821eee3be9e2ab4
annaS <dupa113@dupa.pl>
2018-05-09 19:47:00 +0200

    final commit

--- index.php
+++ index.php
@@ -10,10 +10,11 @@
 <html>
 <head>
 	<title>User Page</title>
+	<link rel="stylesheet" href="css.css" />
 </head>
 <body>
-	<h1>User Page</h1>
-	<h2>Welcome, <?php echo $_SESSION['username']; ?></h2>
+	<h2>Page personnelle</h2>
+	<h2>Bienvenue cher.e <?php echo $_SESSION['username']; ?></h2>
 	<hr>
 	<a href="logout.php">Logout</a>
 </body>
--- login.php
+++ login.php
@@ -1,18 +1,26 @@
 <?php
+
+	if (isset($_POST['rememberMe'])) {
+	    if (isset($_POST ['login'])) {
+	    	setcookie("username111",$_POST['username'], time()+3600);
+	    	setcookie("password111",$_POST['password'], time()+3600);
+	   }
+	}
+
 	$error = false;
 
 	if(isset($_POST['login'])) {
 
-    $users = simplexml_load_file('database.xml');
-    $usersArray = [];
-    for ($i = 0; $i < count($users); $i++) {
-        $userNew = trim($users->person[$i]->username);
-        $passwordNew = trim($users->person[$i]->psw);
-        $usersArray[$userNew] = $passwordNew;
-    }
+	    $users = simplexml_load_file('database.xml');
+	    $usersArray = [];
+	    for ($i = 0; $i < count($users); $i++) {
+	        $userNew = trim($users->person[$i]->username);
+	        $passwordNew = trim($users->person[$i]->psw);
+	        $usersArray[$userNew] = $passwordNew;
+	    }
 
-    $username = $_POST['username'];
-    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+	    $username = $_POST['username'];
+	    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
 
 		foreach ($usersArray as $key => $value) {
@@ -31,18 +39,23 @@
 <html>
 <head>
 	<title>login</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="css.css" />
 </head>
 <body>
-	<h1>login</h1>
+	<h2>Apprenant.e, connecte toi</h2>
 	<form method="post" action="">
-		<p>username <input type="text" name="username"></p>
-		<p>password <input type="password" name="password"></p>
+		<label for="username"><b>Nom d'utilisateur</b></label>
+		<input type="text" name="username" value="<?php if(isset($_COOKIE['username111'])){ echo $_COOKIE['username111'];}?>" required><br><br>
+		<label for="password"><b> Mot de passe </b></label>
+		<input type="password" name="password" value="<?php if(isset($_COOKIE['password111'])){ echo $_COOKIE['password111'];}?>" required><br><br>
 		<?php
 			if($error) {
 				echo '<p>Invalid username and/or password</p>';
 			}
 		?>
-		<p><input type="submit" value="Login" name="login"></p>
+		<input type="checkbox" name="rememberMe" title="Nous utilisons des cookies, cette option n'est donc pas 100% sécurisée"/>Se souvenir de mon nom d'utilisateur et de mon mot de passe<br>
+		<p><input type="submit" value="Se connecter" name="login"></p>
 	</form>
 </body>
 </html>
